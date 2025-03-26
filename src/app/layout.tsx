b94cf037d5d718db42bf9c05b050a484d88b8be6
Victor Machado <victoraugusto16@gmail.com>
2025-03-26 15:36:06 +0100

    feat: install and configure Radix UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+import { Theme } from '@radix-ui/themes'
+import '@radix-ui/themes/styles.css'
 import './globals.css'
 
 const geistSans = Geist({
@@ -25,7 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <Theme>
+          <main>{children}</main>
+        </Theme>
       </body>
     </html>
   )
