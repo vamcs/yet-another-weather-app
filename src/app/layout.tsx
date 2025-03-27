6deea778160f312a8c06238588bf20c2baa5052f
Victor Machado <victoraugusto16@gmail.com>
2025-03-27 21:05:49 +0100

    fix: make content take full height and fix footer at the bottom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,20 +24,22 @@ export default function RootLayout({
     <html lang="en">
       <body className={nunito.variable}>
         <Theme accentColor="sky" radius="large">
-          <Header />
-          <main>{children}</main>
-          <footer>
-            <Container px="6" py="4" size="3">
-              <Flex justify="center">
-                <Text>
-                  Enjoy the weather! Made by{' '}
-                  <Code asChild>
-                    <Link href="https://github.com/vamcs">@vamcs</Link>
-                  </Code>
-                </Text>
-              </Flex>
-            </Container>
-          </footer>
+          <Flex direction="column" height="100svh">
+            <Header />
+            <main>{children}</main>
+            <footer>
+              <Container px="6" py="4" size="3">
+                <Flex justify="center">
+                  <Text>
+                    Enjoy the weather! Made by{' '}
+                    <Code asChild>
+                      <Link href="https://github.com/vamcs">@vamcs</Link>
+                    </Code>
+                  </Text>
+                </Flex>
+              </Container>
+            </footer>
+          </Flex>
         </Theme>
       </body>
     </html>
