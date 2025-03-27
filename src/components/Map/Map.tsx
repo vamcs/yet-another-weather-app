d4affb5f314d41d655654f71b084d084067f817f
Victor Machado <victoraugusto16@gmail.com>
2025-03-27 21:06:10 +0100

    fix: add loading skeleton for Map to avoid CLS

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { Box } from '@radix-ui/themes'
 import { Icon } from 'leaflet'
 import MarkerIcon2X from 'leaflet/dist/images/marker-icon-2x.png'
 import MarkerIcon from 'leaflet/dist/images/marker-icon.png'
@@ -19,19 +18,17 @@ Icon.Default.mergeOptions({
 
 export function Map({ lat, lng }: MapProps) {
   return (
-    <Box height="300px">
-      <MapContainer
-        center={[lat, lng]}
-        zoom={13}
-        scrollWheelZoom={false}
-        className={styles.map}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
-        />
-        <Marker position={[lat, lng]} />
-      </MapContainer>
-    </Box>
+    <MapContainer
+      center={[lat, lng]}
+      zoom={13}
+      scrollWheelZoom={false}
+      className={styles.map}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
+      />
+      <Marker position={[lat, lng]} />
+    </MapContainer>
   )
 }
--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -2,9 +2,11 @@
 
 import dynamic from 'next/dynamic'
 import { MapProps } from './types'
+import { Skeleton } from '@radix-ui/themes'
 
 const Map = dynamic(() => import('./Map').then((mod) => mod.Map), {
   ssr: false,
+  loading: () => <Skeleton height="300px" width="100%" />,
 })
 
 export function DynamicMap(props: MapProps) {
