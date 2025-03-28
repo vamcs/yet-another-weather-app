d2e1d260d8a2616f0347fb59bacfb6d1e461e268
Victor Machado <victoraugusto16@gmail.com>
2025-03-28 01:01:31 +0100

    chore: add empty my locations page for fun

---
+++ src/app/my-locations/page.tsx
@@ -0,0 +1,6 @@
+import { Code } from '@radix-ui/themes'
+
+export default function MyLocationsPage() {
+  // eslint-disable-next-line react/jsx-no-comment-textnodes
+  return <Code color="red">// TODO: Implement</Code>
+}
